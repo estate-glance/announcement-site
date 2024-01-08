eec56515f075937c388884645d2551e680bd958e
Neel Jepaliya <najepaliya@gmail.com>
2024-01-08 01:17:49 -0700

    temporary card impl

--- source/app/app.jsx
+++ source/app/app.jsx
@@ -15,6 +15,9 @@ import {
 } from "./app.module.css";
 import logoName from "/logo-name.svg";
 import logo from "/logo.svg";
+import upload from "/upload.svg";
+import style from "../../public/style.svg";
+import match from "../../public/match.svg";
 
 export function App() {
   const [pageScrolled, setPageScrolled] = useState(false);
@@ -43,6 +46,64 @@ export function App() {
           <p className={`${blueColored} ${heading}`}>Find Your</p>
           <p className={`${pinkColored} ${heading}`}>Dream Home</p>
           <p className={`${coralColored} ${heading}`}>Through Images</p>
+        </div>
+      </div>
+
+      {/* second section */}
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(3, 1fr)",
+          maxWidth: 1500,
+          margin: "0 auto",
+          minHeight: 500,
+          gap: "1rem",
+        }}
+      >
+        <div
+          style={{
+            flex: "1",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            fontSize: "2rem",
+            textAlign: "center",
+          }}
+        >
+          <img src={upload} style={{ maxWidth: "100%" }}></img>
+          <p className={`${coralColored} ${heading}`}>Upload</p>
+          <p>Include images of the homes you love in your home search</p>
+        </div>
+        <div
+          style={{
+            flex: "1",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            fontSize: "2rem",
+            textAlign: "center",
+          }}
+        >
+          <img src={style} style={{ maxWidth: "100%" }}></img>
+          <p className={`${blueColored} ${heading}`}>Style</p>
+          <p>
+            Filter by features, amenities, lighting, and architectural styles.
+            Search anything.
+          </p>
+        </div>
+        <div
+          style={{
+            flex: "1",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            fontSize: "2rem",
+            textAlign: "center",
+          }}
+        >
+          <img src={match} style={{ maxWidth: "100%" }}></img>
+          <p className={`${pinkColored} ${heading}`}>Match</p>
+          <p>Rank family priorities and see top % matches</p>
         </div>
       </div>
     </>
